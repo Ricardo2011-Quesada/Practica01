eb8c4709d242626542b179edd7d034d24ecc9c48
Wenndy <wenndy29@hotmail.com>
2023-10-22 18:28:04 -0600

    Primer commit
    Creación y conexión de la base de datos.

---
+++ arbol01/src/main/java/com/arbol_01/ArbolApplication.java
@@ -0,0 +1,13 @@
+package com.arbol_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbolApplication.class, args);
+	}
+
+}
